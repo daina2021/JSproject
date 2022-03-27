a9c3ecb6d20f1444bc3cfaca4a29b3c839e043d1
unknown <ostrovskadaina7@gmail.com>
2022-03-27 20:10:59 +0300

    create object personaMovieDB

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 "use strict";
-const obj = {
-    name: "Mary",
-    age: 25,
-    isMarried: false,
-};
-console.log(obj.name);
+// const obj = {
+//     name: "Mary",
+//     age: 25,
+//     isMarried: false,
+// };
+// console.log(obj.name);
 
 // alert('daina');
-const result = confirm("Are you here?");
-console.log(result);
+// const result = confirm("Are you here?");
+// console.log(result);
+const numberOfFilms = +prompt("Cik filmas jūs skatījāties?", "");
+const personaMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: true,
+};
+const a = prompt("Movies name?", ""),
+b = +prompt("Balles?", ""),
+c = prompt("Movies name?", ""),
+d = +prompt("Balles?", "");
+
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+console.log(personaMovieDB);
