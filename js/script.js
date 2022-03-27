8439f0db924df0acd4bc28f896d2369635ec1e6e
unknown <ostrovskadaina7@gmail.com>
2022-03-27 17:43:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+const obj = {
+    name: "Mary",
+    age: 25,
+    isMarried: false,
+};
+console.log(obj.name);
+
+// alert('daina');
+const result = confirm("Are you here?");
+console.log(result);
